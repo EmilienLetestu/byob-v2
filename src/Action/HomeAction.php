ed6a5cab404022c06403518fd7465ba29cd1b768
emilien letestu <emilien@pc32.home>
2018-07-26 09:19:18 +0200

    turn default project config into ADR friendly config

---
+++ src/Action/HomeAction.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emilien
+ * Date: 26/07/2018
+ * Time: 09:04
+ */
+
+namespace App\Action;
+
+
+use App\Responder\HomeResponder;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeAction
+
+{
+    /**
+     * @param HomeResponder $responder
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     *
+     * @Route("/", name = "home")
+     */
+    public function __invoke(HomeResponder $responder): Response
+    {
+       return $responder('it works');
+    }
+}
---
+++ src/Responder/HomeResponder.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emilien
+ * Date: 26/07/2018
+ * Time: 09:04
+ */
+
+namespace App\Responder;
+
+
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+
+class HomeResponder
+{
+    private $twig;
+
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    /**
+     * @param string $title
+     * @return Response
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     */
+    public function __invoke(string $title): Response
+    {
+        return new Response(
+            $this->twig->render('home.html.twig',[
+                'title' => $title
+            ])
+        );
+    }
+}
