d395f3bf93f8e9d279bf1b001d660ae8e349f932
emilien letestu <emilien@pc32.home>
2018-07-27 17:02:29 +0200

    [entity] setters and getters + relationship for "order" and "inOrderProduct"

--- src/Entity/InOrderProduct.php
+++ src/Entity/InOrderProduct.php
@@ -21,4 +21,62 @@ class InOrderProduct
      */
     private $quantity;
 
+    /**
+     * @var
+     */
+    private $product;
+
+    /**
+     * @var
+     */
+    private $order;
+
+
+    /**
+     * @param int $quantity
+     */
+    public  function setQuantity(int $quantity): void
+    {
+        $this->quantity = $quantity;
+    }
+
+    /**
+     * @param Product $product
+     */
+    public function setProduct(Product $product): void
+    {
+        $this->product = $product;
+    }
+
+    /**
+     * @param Order $order
+     */
+    public function setOrder(Order $order): void
+    {
+        $this->order = $order;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return Product
+     */
+    public function getProduct(): Product
+    {
+        return $this->product;
+    }
+
+    /**
+     * @return Order
+     */
+    public function getOrder(): Order
+    {
+        return $this->order;
+    }
 }
--- src/Entity/Order.php
+++ src/Entity/Order.php
@@ -9,6 +9,8 @@
 namespace App\Entity;
 
 
+use Doctrine\Common\Collections\ArrayCollection;
+
 class Order
 {
     /**
@@ -40,5 +42,172 @@ class Order
      * @var
      */
     private $status;
+
+    /**
+     * @var
+     */
+    private $orderedBy;
+
+    /**
+     * @var
+     */
+    private $orderedFor;
+
+    /**
+     * @var
+     */
+    private $inOrderProducts;
+
+    /**
+     * @param string $prefix
+     */
+    public function setReference(string $prefix): void
+    {
+        $this->reference = uniqid($prefix);
+    }
+
+    /**
+     * @param string $format
+     */
+    public function setOrderedOn(string $format): void
+    {
+        $this->orderedOn = new \DateTime(date($format));
+    }
+
+    /**
+     * @param \DateTime|null $deliveredOn
+     */
+    public function setDeliveredOn(?\DateTime $deliveredOn): void
+    {
+        $this->deliveredOn = $deliveredOn;
+    }
+
+    /**
+     * @param \DateTime|null $payedOn
+     */
+    public function setPayedOn(?\DateTime $payedOn): void
+    {
+        $this->payedOn = $payedOn;
+    }
+
+    /**
+     * @param string $status
+     */
+    public function setStatus(string $status): void
+    {
+        $this->status = $status;
+    }
+
+    /**
+     * @param User $user
+     */
+    public function setOrderedBy(User $user): void
+    {
+        $this->orderedBy = $user;
+    }
+
+    /**
+     * @param Person $person
+     */
+    public function setOrderedFor(Person $person): void
+    {
+        $this->orderedFor = $person;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getReference(): string
+    {
+        return $this->reference;
+    }
+
+
+    public function getOrderedOn(): \DateTime
+    {
+        return $this->orderedOn;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDeliveredOn():? \DateTime
+    {
+        return $this->deliveredOn;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPayedOn():? \DateTime
+    {
+        return $this->payedOn;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOrderedBy(): User
+    {
+        return $this->orderedBy;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOrderedFor(): Person
+    {
+        return $this->orderedFor;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStatus(): string
+    {
+        return $this->status;
+    }
+
+    /*----------------------- Array Collections -----------------------*/
+
+    /**
+     * Order constructor.
+     */
+    public function __construct()
+    {
+        $this->inOrderProducts = new ArrayCollection();
+    }
+
+    /**
+     * @param InOrderProduct $inOrderProduct
+     */
+    public function addInOrderProduct(InOrderProduct $inOrderProduct)
+    {
+        $this->inOrderProducts[] = $inOrderProduct;
+    }
+
+    /**
+     * @param InOrderProduct $inOrderProduct
+     */
+    public function removeInOrderProduct(InOrderProduct $inOrderProduct)
+    {
+        $this->inOrderProducts->removeElement($inOrderProduct);
+    }
+
+    /**
+     * @return ArrayCollection
+     */
+    public function getInOrderProducts(): ArrayCollection
+    {
+        return $this->inOrderProducts;
+    }
 
 }
