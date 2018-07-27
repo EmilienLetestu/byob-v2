b60d2353822f2f787d33ed3a899bbc5513aca084
emilien letestu <emilien@pc32.home>
2018-07-27 17:35:37 +0200

    [entity] setters and getters for "InStockProduct"

--- src/Entity/InStockProduct.php
+++ src/Entity/InStockProduct.php
@@ -8,7 +8,14 @@
 
 namespace App\Entity;
 
-
+/**
+ * where all referenced product are "turned" into stock with a level and a level alert
+ * where for a given product we will calculate and adjust the flows
+ * where we will keep track of the flows
+ *
+ * Class InStockProduct
+ * @package App\Entity
+ */
 class InStockProduct
 {
     /**
@@ -19,10 +26,72 @@ class InStockProduct
     /**
      * @var
      */
-    private $quantity;
+    private $level = null;
 
     /**
      * @var
      */
     private $alertLevel;
+
+    /**
+     * @var
+     */
+    private $product;
+
+    /**
+     * @param int $level
+     */
+    public function setLevel(?int $level): void
+    {
+        $this->level = $level;
+    }
+
+    /**
+     * @param mixed $alertLevel
+     */
+    public function setAlertLevel($alertLevel): void
+    {
+        $this->alertLevel = $alertLevel;
+    }
+
+    /**
+     * @param Product $product
+     */
+    public function setProduct(Product $product): void
+    {
+        $this->product = $product;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getLevel():? int
+    {
+        return $this->level;
+    }
+
+    /**
+     * @return int
+     */
+    public function getAlertLevel(): int
+    {
+        return $this->alertLevel;
+    }
+
+    /**
+     * @return Product
+     */
+    public function getProduct(): Product
+    {
+        return $this->product;
+    }
+
 }
