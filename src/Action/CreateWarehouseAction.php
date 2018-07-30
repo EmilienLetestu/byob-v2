7daa7f2486acda8ebafaaed7152cacf3ac4a432a
emilien letestu <emilien@pc32.home>
2018-07-30 13:58:13 +0200

    [crud] warehouse creation empty action and responder

---
+++ src/Action/CreateWarehouseAction.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emilien
+ * Date: 30/07/2018
+ * Time: 13:28
+ */
+
+namespace App\Action;
+
+
+class CreateWarehouseAction
+{
+
+}
---
+++ src/CreateWarehouseResponder.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emilien
+ * Date: 30/07/2018
+ * Time: 13:27
+ */
+
+namespace App;
+
+
+class CreateWarehouseResponder
+{
+
+}
