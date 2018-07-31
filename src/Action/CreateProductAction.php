91d436051d08d59ec5961e011db9534e9c59687a
emilien letestu <emilien@pc32.home>
2018-07-31 22:39:01 +0200

    [crud] complete product creation system

--- src/Action/CreateProductAction.php
+++ src/Action/CreateProductAction.php
@@ -9,7 +9,90 @@
 namespace App\Action;
 
 
+use App\Form\Type\ProductType;
+
+use App\Handler\ProductFormHandler;
+use App\Responder\CreateProductResponder;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
 class CreateProductAction
 {
+    /**
+     * @var FormFactoryInterface
+     */
+    private $formFactory;
 
+    /**
+     * @var
+     */
+    private $handler;
+
+    /**
+     * @var
+     */
+    private $urlGenerator;
+
+    /**
+     * @var
+     */
+    private $session;
+
+
+    /**
+     * CreateProductAction constructor.
+     * @param FormFactoryInterface $formFactory
+     * @param ProductFormHandler $handler
+     * @param UrlGeneratorInterface $urlGenerator
+     * @param SessionInterface $session
+     */
+    public function __construct(
+        FormFactoryInterface  $formFactory,
+        ProductFormHandler    $handler,
+        UrlGeneratorInterface $urlGenerator,
+        SessionInterface      $session
+
+    )
+    {
+        $this->formFactory  = $formFactory;
+        $this->handler      = $handler;
+        $this->urlGenerator = $urlGenerator;
+        $this->session      = $session;
+    }
+
+    /**
+     * @Route("/produit/ajouter", name = "createProduct")
+     *
+     * @param Request $request
+     * @param CreateProductResponder $responder
+     * @return Response
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     */
+    public function __invoke(Request $request, CreateProductResponder $responder): Response
+    {
+       $form = $this->formFactory
+           ->create(ProductType::class)
+           ->handleRequest($request)
+       ;
+
+       if($this->handler->handle($form))
+       {
+           $this->session->getFlashBag()
+               ->add('success', 'Produit ajouter avec succès')
+           ;
+
+           return new RedirectResponse(
+               $this->urlGenerator->generate('dashboard')
+           );
+       }
+
+       return $responder($form->createView());
+    }
 }
--- src/Responder/CreateProductResponder.php
+++ src/Responder/CreateProductResponder.php
@@ -9,7 +9,40 @@
 namespace App\Responder;
 
 
+use Symfony\Component\Form\FormView;
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+
 class CreateProductResponder
 {
+    /**
+     * @var Environment
+     */
+    private $twig;
 
+    /**
+     * CreateProductResponder constructor.
+     * @param Environment $twig
+     */
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    /**
+     * @param FormView $form
+     * @return Response
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     */
+    public function __invoke(FormView $form): Response
+    {
+       return new Response(
+           $this->twig->render('create_entity.html.twig',[
+               'form' => $form,
+               'formTemplate' => 'form/product_form.html.twig'
+           ])
+       );
+    }
 }
