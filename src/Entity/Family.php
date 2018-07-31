3b91f6f013220e8bcc14da903f4cba883be4fe42
emilien letestu <emilien@pc32.home>
2018-07-31 15:37:15 +0200

    [entity] add  all required entities to ease product management => have to fil'em up

---
+++ src/Entity/Family.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emilien
+ * Date: 31/07/2018
+ * Time: 15:33
+ */
+
+namespace App\Entity;
+
+
+class Family
+{
+
+}
---
+++ src/Entity/Type.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emilien
+ * Date: 31/07/2018
+ * Time: 15:34
+ */
+
+namespace App\Entity;
+
+
+class Type
+{
+
+}
