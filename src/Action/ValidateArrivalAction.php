b5375268ebd9edeb5649ce260f16a6b2b58f161f
emilien letestu <emilien@pc32.home>
2018-08-06 23:07:58 +0200

    begin to work on product arrival validation

---
+++ src/Action/ValidateArrivalAction.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emilien
+ * Date: 06/08/2018
+ * Time: 22:47
+ */
+
+namespace App\Action;
+
+
+class ValidateArrivalAction
+{
+
+}
---
+++ src/Responder/ValidateArrivalResponder.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emilien
+ * Date: 06/08/2018
+ * Time: 22:48
+ */
+
+namespace App\Responder;
+
+
+class ValidateArrivalResponder
+{
+
+}
