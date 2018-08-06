a9a13701f7ef185dbbc7f97d0e92a14f9cfb87d6
emilien letestu <emilien@pc32.home>
2018-08-07 01:21:53 +0200

    fully functional product arrival validation system

--- src/Action/ValidateArrivalAction.php
+++ src/Action/ValidateArrivalAction.php
@@ -9,7 +9,49 @@
 namespace App\Action;
 
 
+use App\Responder\ValidateArrivalResponder;
+use App\Services\ArrivalValidation;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
 class ValidateArrivalAction
 {
+    private $token;
 
+    private $arrivalValidation;
+
+    public function __construct(
+        TokenStorageInterface $token,
+        ArrivalValidation     $arrivalValidation
+    )
+    {
+        $this->token = $token;
+        $this->arrivalValidation = $arrivalValidation;
+    }
+
+    /**
+     * @Route(
+     *     "valider/arrivage/{pendingId}",
+     *      name = "validateArrival",
+     *     requirements={"pendingId" = "\d+"}
+     * )
+     * @Route(
+     *     "refuser/arrivage/{pendingId}",
+     *      name = "rejectArrival",
+     *      requirements={"pendingId" = "\d+"}
+     * )
+     *
+     * @param Request $request
+     */
+    public function __invoke(Request $request, ValidateArrivalResponder $responder)
+    {
+        $user = $this->token->getToken()->getUser();
+        $pendingId = $request->get('pendingId');
+        $route    = $request->attributes->get('_route');
+
+        $this->arrivalValidation->ValidateOrReject($route, $pendingId, $user);
+
+        return $responder();
+    }
 }
--- src/Responder/ValidateArrivalResponder.php
+++ src/Responder/ValidateArrivalResponder.php
@@ -9,7 +9,29 @@
 namespace App\Responder;
 
 
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
 class ValidateArrivalResponder
 {
+    /**
+     * @var UrlGeneratorInterface
+     */
+    private  $urlGenerator;
 
+    /**
+     * ValidateArrivalResponder constructor.
+     * @param UrlGeneratorInterface $urlGenerator
+     */
+    public function __construct(UrlGeneratorInterface $urlGenerator)
+    {
+        $this->urlGenerator = $urlGenerator;
+    }
+
+    public function __invoke()
+    {
+       return new RedirectResponse(
+           $this->urlGenerator->generate('dashboard')
+       );
+    }
 }
