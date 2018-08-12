964e1a0e98950fa09589b83ed17b0a8aeabba067
emilien letestu <emilien@pc32.home>
2018-08-12 16:54:20 +0200

    fully functional customercreation system

---
+++ src/Action/CreateCustomerAction.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emilien
+ * Date: 12/08/2018
+ * Time: 16:02
+ */
+
+namespace App\Action;
+
+
+use App\Form\Type\CreateCustomerType;
+use App\Handler\CreateCustomerHandler;
+use App\Responder\CreateCustomerResponder;
+
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CreateCustomerAction
+{
+    /**
+     * @var FormFactoryInterface
+     */
+    private $formFactory;
+
+    /**
+     * @var CreateCustomerHandler
+     */
+    private $handler;
+
+    /**
+     * @var UrlGeneratorInterface
+     */
+    private $urlGenerator;
+
+    /**
+     * @var SessionInterface
+     */
+    private $session;
+
+    /**
+     * CreateCustomerAction constructor.
+     * @param FormFactoryInterface $formFactory
+     * @param CreateCustomerHandler $handler
+     * @param UrlGeneratorInterface $urlGenerator
+     * @param SessionInterface $session
+     */
+    public function __construct(
+        FormFactoryInterface  $formFactory,
+        CreateCustomerHandler $handler,
+        UrlGeneratorInterface $urlGenerator,
+        SessionInterface      $session
+    )
+    {
+        $this->formFactory  = $formFactory;
+        $this->handler      = $handler;
+        $this->urlGenerator = $urlGenerator;
+        $this->session      = $session;
+    }
+
+    /**
+     * @Route("/client/ajouter", name="createCustomer")
+     *
+     * @param Request $request
+     * @param CreateCustomerResponder $responder
+     * @return Response
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     */
+    public function __invoke(Request $request, CreateCustomerResponder $responder): Response
+    {
+        $form = $this->formFactory
+            ->create(CreateCustomerType::class)
+            ->handleRequest($request)
+        ;
+
+        if($this->handler->handle($form))
+        {
+            $this->session->getFlashBag()
+                ->add('success', 'Client ajouté avec succès')
+            ;
+
+            return new RedirectResponse(
+                $this->urlGenerator->generate('customerList')
+            );
+        }
+
+        return $responder(
+            $form->createView()
+        );
+    }
+}
---
+++ src/Handler/CreateCustomerHandler.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emilien
+ * Date: 12/08/2018
+ * Time: 16:06
+ */
+
+namespace App\Handler;
+
+
+use App\Entity\Contact;
+use App\Entity\Customer;
+use App\Entity\Person;
+use App\Handler\Interfaces\EntityFormHandlerInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+
+class CreateCustomerHandler implements EntityFormHandlerInterface
+{
+    /**
+     * @var
+     */
+    private $doctrine;
+
+    /**
+     * @var
+     */
+    private $tokenStorage;
+
+    /**
+     * @var
+     */
+    private $customer;
+
+    /**
+     * @var
+     */
+    private $person;
+
+
+    /**
+     * CreateCustomerHandler constructor.
+     * @param EntityManagerInterface $doctrine
+     * @param TokenStorageInterface $tokenStorage
+     */
+    public function __construct(
+        EntityManagerInterface $doctrine,
+        TokenStorageInterface  $tokenStorage
+    )
+    {
+        $this->doctrine     = $doctrine;
+        $this->tokenStorage = $tokenStorage;
+        $this->customer     = new Customer();
+        $this->person       = new Person();
+    }
+
+    /**
+     * @param FormInterface $form
+     * @return bool
+     */
+    public function handle(FormInterface $form): bool
+    {
+       if($form->isSubmitted() && $form->isValid())
+       {
+           $this->customer->setCompany($form->get('company')->getData());
+           $this->customer->setAddedBy(
+               $this->tokenStorage->getToken()->getUser()
+           );
+           $this->customer->setAddedOn('Y-m-d');
+
+           $this->doctrine->persist($this->customer);
+
+           $this->person->setCustomer($this->customer);
+           $this->person->setFullName($form->get('fullname')->getData());
+           $this->person->setJob($form->get('job')->getData());
+
+           $this->doctrine->persist($this->person);
+
+           $contact = new Contact();
+
+           $contact->setType('email');
+           $contact->setData($form->get('email')->getData());
+           $contact->setPerson($this->person);
+
+           $this->doctrine->persist($contact);
+
+           if($form->get('mobile')->getData() !== null)
+           {
+               $mobile = new Contact();
+               $mobile->setType('mobile');
+               $mobile->setData($form->get('mobile')->getData());
+               $mobile->setPerson($this->person);
+
+               $this->doctrine->persist($mobile);
+           }
+
+           if($form->get('phone')->getData() !== null)
+           {
+               $phone = new Contact();
+               $phone->setType('phone');
+               $phone->setData($form->get('phone')->getData());
+               $phone->setPerson($this->person);
+
+               $this->doctrine->persist($phone);
+           }
+
+           $this->doctrine->flush();
+
+          return true;
+       }
+
+       return false;
+    }
+}
